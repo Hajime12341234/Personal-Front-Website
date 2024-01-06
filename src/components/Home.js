6dad7d5af6b1e96c2c7154e347789243d4a4916a
Hajime12341234 <132004336+Hajime12341234@users.noreply.github.com>
2024-01-06 01:50:18 -0500

    Update Home.js
    Adding contents to About-Me page

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,17 +16,33 @@ const Home = () => {
                 <div className="about-info">
                     <p>
                         Hi! I'm Hajime Saeki. I'm currently a third-year undergraduate in computer science 
-                        and mathematics at the University of Toronto. 
-                        Your introduction goes here. Provide information about yourself, 
-                        your interests, and anything else you'd like. 
+                        and mathematics at University of Toronto. My focuses and interests mainly encompass 
+                        abstract algebra, analysis and coding based problem-solving. When I have free time, 
+                        I would be enjoying to learn some from these. And 
+                        I like to learn and get impressed by cool things lurking in mathematics.  
                     </p>
 
                     <p>
-                        Hi! I'm Hajime Saeki. I'm currently an undergraduate in computer science and mathematics
-                        at the University of Toronto. 
-                        Your introduction goes here. Provide information about yourself, 
-                        your interests, and anything else you'd like. 
+                        Put simply, I like to deeply immerse myself into the problems and thoughts whose answers do not exist or are not trivial. 
+                        Those thoughts of mine often associate with cool stuff and intuition in mathematics. I feel that real creativity 
+                        exists in the high level of abstraction, which I believe also lies in the world of mathematics. 
                     </p>
+
+                    <p>
+                        I have not learned well enough to know anything. However, accumulated ideas and 
+                        intuitions gained from learning experiences would eventually gauge into one 
+                        realization, which you would never expect to encounter.
+                        I believe learning is never wasted and is conducted through the collision of accumulated ideas. 
+                    </p>
+                    <p>
+                        I find the web development quite challenging and not enjoyable. 
+                        Not only that, computer science could have been what I would never care to do in life. 
+                        However, I ended up giving it a shot and there have already been so much to learn. 
+                        what you're seeing now is the product of one of my 
+                        "would never have done that, hence I give it a shot" journeys. 
+                            
+                    </p>
+
                 </div>
             </div>
             <div className="about-info">
